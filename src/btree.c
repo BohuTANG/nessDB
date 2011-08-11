e0809828dcadad9f61d3a0c68455ff676268e905
overred <overred@gmail.com>
2011-08-11 13:36:28 +0800

    fixed split bug

--- src/btree.c
+++ src/btree.c
@@ -12,9 +12,9 @@
 #define L1_IDX_NAME 	"level_1.idx"
 #define DB_NAME		"dump.db"
 
-#define BUCKET_SIZE 	(sizeof(bucket_t))
+#define PAGE_SIZE 	(sizeof(page_t))
 #define X 		(29989)
-#define ROOT_GAP 	(((X/BUCKET_SIZE)+1)*BUCKET_SIZE)
+#define ROOT_GAP 	(((X/PAGE_SIZE)+1)*PAGE_SIZE)
 
 typedef struct blob
 {
@@ -36,7 +36,7 @@ typedef struct btree
 
 static btree_t *_btree;
 
-static unsigned int bucket_hash(char* str)
+static unsigned int slot_hash(char* str)
 {
 	unsigned int hash=5381;
 	int c;
@@ -44,17 +44,17 @@ static unsigned int bucket_hash(char* str)
 		hash=((hash<<5)+hash)+c;//* hash*33+c */
 		
    	unsigned int mod=hash%X;	
-	unsigned int l=mod/BUCKET_SIZE;
-	mod=(l+1)*BUCKET_SIZE;
+	unsigned int l=mod/PAGE_SIZE;
+	mod=(l+1)*PAGE_SIZE;
 	return mod;
 }
 
-static void flush_bucket(bucket_t *bucket,size_t bucket_offset,blob_t *blob)
+static void flush_data(page_t *page,size_t page_offset,blob_t *blob)
 {
-	//bucket
-	fseek(_btree->idx_w,bucket_offset,SEEK_SET);
-	fwrite(bucket,sizeof(bucket_t),1,_btree->idx_w);
-	_btree->idx_offset+=sizeof(bucket_t);
+	//page
+	fseek(_btree->idx_w,page_offset,SEEK_SET);
+	fwrite(page,sizeof(page_t),1,_btree->idx_w);
+	_btree->idx_offset+=sizeof(page_t);
 
 	//value	
 	fseek(_btree->db_w,_btree->db_offset,SEEK_SET);
@@ -62,33 +62,134 @@ static void flush_bucket(bucket_t *bucket,size_t bucket_offset,blob_t *blob)
 	_btree->db_offset+=blob->len;
 }
 
-static void  split_bucket(bucket_t *bucket,size_t bucket_offset)
+static void  split_page(page_t *page,size_t page_offset)
 {
-	int new_used=bucket->used/2;
+	int new_used=page->used/2;
 	int child=_btree->idx_offset;
 
 
-	//write new child  bucket
-	bucket_t *new_bucket=(bucket_t*)malloc(sizeof(bucket_t));
-	new_bucket->used=(BLOCK_NUM-new_used);
-	memcpy(new_bucket->blocks,&bucket->blocks[new_used],(BLOCK_NUM-new_used)*BLOCK_SIZE);
+	//write new child  page
+	page_t *new_page=(page_t*)malloc(sizeof(page_t));
+	new_page->used=(BLOCK_NUM-new_used);
+	printf("used:%d,new_used:%d",page->used,new_used);
+
+	memcpy(new_page->blocks,&page->blocks[new_used],(BLOCK_NUM-new_used)*BLOCK_SIZE);
 	fseek(_btree->idx_w,0,SEEK_END);
-	fwrite(new_bucket,sizeof(bucket_t),1,_btree->idx_w);
-	if(new_bucket)
-		free(new_bucket);
+	fwrite(new_page,sizeof(page_t),1,_btree->idx_w);
+	if(new_page)
+		free(new_page);
 
 	//update
-	_btree->idx_offset+=sizeof(bucket_t);
+	_btree->idx_offset+=sizeof(page_t);
 
-	//write old bucket
-	bucket->used=new_used;
-	bucket->blocks[new_used-1].child=child;
-	memset(&bucket->blocks[new_used],0,(BLOCK_NUM-new_used)*BLOCK_SIZE);
+	//write old page
+	page->used=new_used;
+	page->blocks[new_used-1].child=child;
 
-	fseek(_btree->idx_w,bucket_offset,SEEK_SET);
-	fwrite(bucket,sizeof(bucket_t),1,_btree->idx_w);
+	memset(&page->blocks[new_used],0,(BLOCK_NUM-new_used+1)*BLOCK_SIZE);
 
-//	printf("new child bucket offset:%d,new-used:%d\n",child,new_used);
+	fseek(_btree->idx_w,page_offset,SEEK_SET);
+	fwrite(page,sizeof(page_t),1,_btree->idx_w);
+
+//	printf("new child page offset:%d,new-used:%d\n",child,new_used);
+}
+
+
+static size_t insert_page(size_t page_offset,char *key,blob_t *blob)
+{
+	page_t* page=(page_t*)malloc(sizeof(page_t));
+
+	fseek(_btree->idx_r,page_offset,SEEK_SET);
+	fread(page,sizeof(page_t),1,_btree->idx_r);
+	block_t* blocks=page->blocks;
+
+	int left=0,right=page->used-1;
+	while(left<=right)
+	{
+		size_t i=(left+right)/2;
+		int cmp=strcmp(key,blocks[i].key);
+		if(cmp==0)
+		{
+			//TODO update
+		}
+		if(cmp<0)
+			right=(i-1);
+		else
+			left=(i+1);
+	}
+
+	size_t i=left;
+	int child=blocks[i].child;
+	if(child)
+	{
+		child=insert_page(child,key,blob);			
+	}	
+
+	memmove(&blocks[i],&blocks[i],sizeof(block_t)*(page->used-i-1));
+
+	page->used+=1;
+
+	block_t *block=&blocks[i];
+	memcpy(block->key,key,KEY_SIZE);
+	block->val_offset=_btree->db_offset;
+	block->val_length=blob->len;
+	block->child=0;
+
+	//flush index and data to file
+	flush_data(page,page_offset,blob);
+	printf("add-->key:%s,slot:%d,used:%d,val_off:%d\n",key,page_offset,page->used,block->val_offset);
+	if(page->used>(BLOCK_NUM-1))
+	{	
+		printf("BLOCK_NUM:%d\n",BLOCK_NUM);
+		split_page(page,page_offset);
+	}
+	
+	//free
+	if(page)
+	{
+		free(page);
+		page=NULL;
+	}
+
+	return child;
+}
+
+
+static size_t lookup(char *key,size_t page_offset)
+{
+	while(page_offset)
+	{
+		page_t* page=(page_t*)malloc(sizeof(page_t));
+		fseek(_btree->idx_r,page_offset,SEEK_SET);
+		fread(page,sizeof(page_t),1,_btree->idx_r);
+		int used=0;
+		block_t* blocks=page->blocks;
+
+		printf("get-->key:%s,slot:%d,used:%d\n",key,page_offset,page->used);
+		int left=0,right=page->used-1;
+		while(left<=right)
+		{
+			size_t i=(left+right)/2;
+			int cmp=strcmp(key,blocks[i].key);
+			if(cmp==0)
+			{
+				int off=blocks[i].val_offset;
+				if(page)
+					free(page);
+		//		printf("get-->key:%s,slot:%d,used:%d,off:%d\n",key,page_offset,page->used,off);
+				return off;
+			}
+			if(cmp<0)
+				right=(i-1);
+			else
+				left=(i+1);
+		}
+		size_t i=left;
+		page_offset=blocks[i].child;
+		if(page)
+			free(page);
+	}
+	return 0;
 }
 
 void btree_init(int capacity)
@@ -126,152 +227,84 @@ void btree_init(int capacity)
 }
 
 
-static size_t insert_bucket(size_t bucket_offset,char *key,blob_t *blob)
-{
-	bucket_t* bucket=(bucket_t*)malloc(sizeof(bucket_t));
-
-	fseek(_btree->idx_r,bucket_offset,SEEK_SET);
-	fread(bucket,sizeof(bucket_t),1,_btree->idx_r);
-	block_t* blocks=bucket->blocks;
-
-	int left=0,right=bucket->used-1;
-	while(left<=right)
-	{
-		size_t i=(left+right)/2;
-		int cmp=strcmp(key,blocks[i].key);
-		if(cmp==0)
-		{
-			//TODO update
-			if(bucket)
-				free(bucket);
-			return bucket_offset;
-		}
-		if(cmp<0)
-			right=(i-1);
-		else
-			left=(i+1);
-	}
-
-	size_t i=left;
-	int child=blocks[i].child;
-	if(child)
-	{
-		child=insert_bucket(child,key,blob);			
-	}	
-
-	memmove(&blocks[i+1],&blocks[i],sizeof(block_t)*(bucket->used-i));
-
-	bucket->used+=1;
-
-	block_t *block=&blocks[i];
-	memcpy(block->key,key,KEY_SIZE);
-	block->val_offset=_btree->db_offset;
-	block->val_length=blob->len;
-	block->child=0;
-
-	//flush index and data to file
-	flush_bucket(bucket,bucket_offset,blob);
-	printf("add-->key:%s,slot:%d,used:%d,val_off:%d\n",key,bucket_offset,bucket->used,block->val_offset);
-	if(bucket->used>(BLOCK_NUM-1))
-		split_bucket(bucket,bucket_offset);
-	
-	//free
-	if(bucket)
-		free(bucket);
-
-	return child;
-}
-
 void btree_add(char *key,char *val)
 {
-	unsigned int h=bucket_hash(key);
+	unsigned int h=slot_hash(key);
 	blob_t *blob=(blob_t*)malloc(sizeof(blob_t));
 	blob->len=strlen(val);
 	blob->val=val;
 
-	insert_bucket(h,key,blob);
+	insert_page(h,key,blob);
 
 	if(blob)
 		free(blob);
 }
 
 
-static size_t lookup(char *key,size_t bucket_offset)
-{
-	while(bucket_offset)
-	{
-		bucket_t* bucket=(bucket_t*)malloc(sizeof(bucket_t));
-		fseek(_btree->idx_r,bucket_offset,SEEK_SET);
-		fread(bucket,sizeof(bucket_t),1,_btree->idx_r);
-		int used=0;
-		block_t* blocks=bucket->blocks;
-
-		printf("get-->key:%s,slot:%d,used:%d\n",key,bucket_offset,bucket->used);
-		int left=0,right=bucket->used-1;
-		while(left<=right)
-		{
-			size_t i=(left+right)/2;
-			int cmp=strcmp(key,blocks[i].key);
-			if(cmp==0)
-			{
-				int off=blocks[i].val_offset;
-				if(bucket)
-					free(bucket);
-		//		printf("get-->key:%s,slot:%d,used:%d,off:%d\n",key,bucket_offset,bucket->used,off);
-				return off;
-			}
-			if(cmp<0)
-				right=(i-1);
-			else
-				left=(i+1);
-		}
-		size_t i=left;
-		bucket_offset=blocks[i].child;
-		if(bucket)
-			free(bucket);
-	}
-	return 0;
-}
-
 int btree_get(char *key,char *val)
 {
-	unsigned int h=bucket_hash(key);
+	unsigned int h=slot_hash(key);
 	size_t offset=lookup(key,h);
 	printf("get offset:%d\n",offset);
 }
 
-
-int main()
+void btree_destroy()
 {
-	int loop=10;
+	fclose(_btree->idx_r);
+	fclose(_btree->idx_w);
+	fclose(_btree->db_r);
+	fclose(_btree->db_w);
+	if(_btree)
+		free(_btree);
+}
+
+
+int main(int argc,char** argv)
+{
+	if(argc!=3)
+	{
+		fprintf(stderr,"Usage: btree <op> <count> \n");
+		exit(1);
+	}
+
+	int loop=atoi(argv[2]);
 	char k[16]="88abcdefghigklmn";
 	char *v="abddddddddddddddddddddddddddddddddddddddabdddddddddddddddddddddddddddddddddddddd";
 
 	btree_init(loop+33);
 	int i=0;
-	clock_t begin=clock(),end;
-#ifdef ADD
-	for(i=0;i<loop;i++)
+	if(strcmp(argv[1],"add")==0)
 	{
-		sprintf(k,"%dabcdefg",i);
-		btree_add(k,v);
-		if((i%10000)==0)
+		clock_t begin=clock(),end;
+		for(i=0;i<loop;i++)
 		{
-			fprintf(stderr,"finished %d ops%30s\r",i,"");
-			fflush(stderr);
+			sprintf(k,"%dabcdefg",i);
+			btree_add(k,v);
+			if((i%10000)==0)
+			{
+				fprintf(stderr,"finished %d ops%30s\r",i,"");
+				fflush(stderr);
+			}
 		}
+		end=clock();
+		double cost=end-begin;
+		printf("add cost:%lf\n",cost);
 	}
-#else
-	for(i=0;i<loop;i++)
-	{
-		char val[1024]={0};
-		sprintf(k,"%dabcdefg",i);
-		btree_get(k,val);
-	}
-#endif
 
-	end=clock();
-	double cost=end-begin;
-	printf("cost:%lf\n",cost);
+	if(strcmp(argv[1],"get")==0)
+	{
+		clock_t begin=clock(),end;
+		for(i=0;i<loop;i++)
+		{
+			char val[1024]={0};
+			sprintf(k,"%dabcdefg",i);
+			btree_get(k,val);
+		}
+		end=clock();
+		double cost=end-begin;
+		printf("get cost:%lf\n",cost);
+	}
+
+	btree_destroy();
 	return 1;	
 }
