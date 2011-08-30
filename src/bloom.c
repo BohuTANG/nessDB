9c71f94568f176bf6f3059fcd44b7f913f249f0f
overred <overred@gmail.com>
2011-08-30 11:37:29 +0800

    add

--- src/bloom.c
+++ src/bloom.c
@@ -3,3 +3,5 @@
 #define SETBIT_1(bitset,i) (bitset[i / CHAR_BIT] |=  (1<<(i % CHAR_BIT)))
 #define SETBIT_0(bitset,i) (bitset[i / CHAR_BIT] &=  (~(1<<(i % CHAR_BIT))))
 #define GETBIT  (bitset,i) (bitset[i / CHAR_BIT] &   (1<<(i % CHAR_BIT)))
+
+
--- src/bloom.h
+++ src/bloom.h
@@ -1,4 +1,16 @@
 #ifndef _BLOOM_H
 #define _BLOOM_H
 
+struct bloom
+{
+	unsigned char	*bitset;
+	unsigned int	size;
+	unsigned int	count;
+	unsigned int 	(*bloom_hashfunc) (const char*);
+};
+
+void	bloom_init(struct bloom *bloom,int  size);
+void	bloom_add(struct bloom *bloom,const char *k);
+int	bloom_get(struct bloom *bloom,const char *k);
+
 #endif
