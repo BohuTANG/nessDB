d0cdfa133c97efc65503b25fb3364b9f8ed8eba0
overred <overred@gmail.com>
2011-09-02 17:18:39 +0800

    add

---
+++ src/memtable.c
@@ -0,0 +1,17 @@
+#include "memtable.h"
+
+void	init(struct memtable *table)
+{
+	//TODO
+}
+
+void	set(struct memtable *table,OP_TYPE)
+{
+	//TODO
+}
+
+uint64_t get(struct memtable *table,const char *k)
+{
+	//TODO
+	return 0;	
+}
---
+++ src/sstable.h
@@ -0,0 +1,4 @@
+#ifndef _SSTABLE_H
+#define _SSTABLE_H
+
+#endif
