d3d3349f1a8a75d66358e52966c76081f84f2c1a
BohuTANG <overred.shuttler@gmail.com>
2011-10-16 11:51:51 +0800

    fix hashtable STRING free

--- src/ht.c
+++ src/ht.c
@@ -132,14 +132,18 @@ void ht_remove(struct ht *ht,void *k)
 void ht_free(struct ht *ht)
 {
 	int i;
-	for(i=0;i<ht->cap;i++){
-		struct ht_node *cur,*nxt;
-		cur=ht->nodes[i];
-		while(cur!=NULL){
-			nxt=cur->next;
-			free(cur->k);
-			free(cur);
-			cur=nxt;
+	if(ht->key_type==STRING)
+		free(ht->nodes);
+	else{
+		for(i=0;i<ht->cap;i++){
+			struct ht_node *cur,*nxt;
+			cur=ht->nodes[i];
+			while(cur!=NULL){
+				nxt=cur->next;
+				free(cur->k);
+				free(cur);
+				cur=nxt;
+			}
 		}
 	}
 }
