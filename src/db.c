17d9cad7829ee1ca12b9543d90fc0305348db5aa
BohuTANG <overred.shuttler@gmail.com>
2011-10-20 13:56:26 +0800

    fixup compile warning when add #define _GNU_SOURCE

--- src/db.c
+++ src/db.c
@@ -25,6 +25,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#define _GNU_SOURCE
 #define _LARGEFILE64_SOURCE
 #include <sys/types.h>
 #include <stdlib.h>
