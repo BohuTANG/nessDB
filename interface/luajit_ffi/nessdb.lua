bfd7a320009557e706e60a79cc17bbfdc8917902
Olivier Goudron <olivier.goudron@gmail.com>
2012-03-03 13:19:19 +0100

    code refactoring

--- interface/luajit_ffi/nessdb.lua
+++ interface/luajit_ffi/nessdb.lua
@@ -1,15 +1,15 @@
-#!/usr/bin/env luajit
--- Quick and dirty low level nessDB LuaJIT FFI binding
--- The main use is for quick nessDB testing, a real binding should be more user friendly
+-- Low level nessDB LuaJIT FFI binding
+-- This binding export only nessDB functions and types
 -- Copyright (C) 2012 Olivier Goudron
 
 local ffi = require("ffi")
+
 ffi.cdef[[
 struct slice{
    char *data;
    int len;
 };
-struct nessdb *db_open(size_t bufferpool_size, const char *basedir, int tolog);
+struct nessdb *db_open(size_t bufferpool_size, const char *basedir, int is_log_recovery);
 int db_get(struct nessdb *db, struct slice *sk, struct slice *sv);
 int db_exists(struct nessdb *db, struct slice *sk);
 int db_add(struct nessdb *db, struct slice *sk, struct slice *sv);
@@ -18,90 +18,46 @@ char *db_info(struct nessdb *db);
 void db_close(struct nessdb *db);
 ]]
 
-local nessdb = ffi.load("../../libnessdb.so") -- libnessdb.so library path
-local kbuf = ffi.new("char[33]") -- max key size in bytes + 1
-local vbuf = ffi.new("char[1025]") -- max value size in bytes + 1
-local sk = ffi.new("struct slice")
-local sv = ffi.new("struct slice")
-sk.data = kbuf
-sv.data = vbuf
+local N = {} -- exported functions and types
 
-local function print_buf(message)
-	print(message .. ffi.string(sk.data) .. " / " .. ffi.string(sv.data))
+local nessdb_n = ffi.load("../../libnessdb.so") -- libnessdb.so namespace
+N.buffer_t = ffi.typeof("char[?]") -- char buffer type
+N.slice_t = ffi.typeof("struct slice") -- struct slice type
+
+
+function N.openlib(lib_path)
+	nessdb_n = ffi.load(lib_path)
 end
 
-local function db_open(path)
-	return nessdb.db_open(10580, path, 1) -- buffer pool size = (max_key_size + max_value_size) * number of LRU slots wanted
+-- buffer_pool_size = (max_key_size + max_value_size) * number of LRU slots wanted
+-- base_dir = name of db directory
+-- is_log_recovery = allow log recovery or not
+function N.open(buffer_pool_size, base_dir, is_log_recovery)
+	return nessdb_n.db_open(buffer_pool_size, base_dir, is_log_recovery)
 end
 
-local function db_add(db, key, value)
-	sk.len = string.len(key)
-	ffi.copy(sk.data, key)
-	sv.len = string.len(value)
-	ffi.copy(sv.data, value)
-	return nessdb.db_add(db, sk, sv)
+function N.get(db, sk, sv)
+	return nessdb_n.db_get(db, sk, sv)
 end
 
-local function db_get(db, key)
-	sk.len = string.len(key)
-	ffi.copy(sk.data, key)
-	return nessdb.db_get(db, sk, sv)
+function N.exists(db, sk)
+	return nessdb_n.db_exists(db, sk)
 end
 
-local function db_exists(db, key)
-	sk.len = string.len(key)
-	ffi.copy(sk.data, key)
-	return nessdb.db_exists(db, sk)
+function N.add(db, sk, sv)
+	return nessdb_n.db_add(db, sk, sv)
 end
 
-local function db_remove(db, key)
-	sk.len = string.len(key)
-	ffi.copy(sk.data, key)
-	nessdb.db_remove(db, sk)
+function N.remove(db, sk)
+	nessdb_n.db_remove(db, sk)
 end
 
-local function db_info(db)
-	return ffi.string(nessdb.db_info(db))
+function N.info(db)
+	return ffi.string(nessdb_n.db_info(db))
 end
 
-local function db_close(db)
-	nessdb.db_close(db)
+function N.close(db)
+	nessdb_n.db_close(db)
 end
 
-
----- test code start here ----
-db = db_open(".")
-
--- testing add
-for n = 1,10 do
-	local exit_code = db_add(db, "key" .. n, "value" .. n)
-	print("Function add : " .. ffi.string(sk.data) .. " / " .. ffi.string(sv.data) .. " > exit_code = " .. exit_code)
-end
-
--- testing get
-for n = 1,10 do
-	local exit_code = db_get(db, "key" .. n)
-	print("Function get : " .. ffi.string(sk.data) .. " / " .. ffi.string(sv.data) .. " > exit_code = " .. exit_code)
-end
-
--- testing exists
-for n = 1,10 do
-	local exit_code = db_exists(db, "key" .. n)
-	print("Function exists : " .. ffi.string(sk.data) .. " > exit_code = " .. exit_code)
-end
-
--- testing remove
-for n = 1,10 do
-	db_remove(db, "key" .. n)
-	print("Function remove : " .. ffi.string(sk.data))
-end
-
--- testing exists again
-for n = 1,10 do
-	local exit_code = db_exists(db, "key" .. n)
-	print("Function exists : " .. ffi.string(sk.data) .. " > exit_code = " .. exit_code)
-end
-
--- db info and close
-print(db_info(db))
-db_close(db)
+return N
