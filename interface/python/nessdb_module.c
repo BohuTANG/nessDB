ddf43f0d251f35dc45783c1f9dbffc9cbe62d4ab
=zhangyanfei <overred.shuttler@gmail.com>
2012-07-17 17:25:22 +0800

    modified python's test case

--- interface/python/nessdb_module.c
+++ interface/python/nessdb_module.c
@@ -12,13 +12,12 @@
 static PyObject *
 pynessdb_db_open(PyObject *self, PyObject *args)
 {
-    int bufferpool_size;
     const char *db_path;
     int is_log_recovery;
     
-    if (!PyArg_ParseTuple(args, "sii", &db_path,&bufferpool_size,&is_log_recovery))
+    if (!PyArg_ParseTuple(args, "si", &db_path,&is_log_recovery))
         return NULL;
-    struct nessdb *ret = db_open(bufferpool_size,db_path,is_log_recovery);
+    struct nessdb *ret = db_open(db_path,is_log_recovery);
     return Py_BuildValue("l", ret);
 }
 
--- interface/python/test.py
+++ interface/python/test.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 # author : KDr2
+# BohuTANG @2012
 #
 
-
-
+import sys
+import random
+import string
+import time
 import nessdb
 
-db=nessdb.NessDB("test.db")
-db.db_add("key","value")
-print db.db_get("key")
-print db.db_exists("key")
-print db.db_exists("key1")
-db.db_close()
+def gen_random_str(len):
+	return ''.join([random.choice('abcdefghijklmnoprstuvwyxzABCDEFGHIJKLMNOPRSTUVWXYZ') for i in range(len)])
 
+def ness_open(db_name):
+	return nessdb.NessDB(db_name)
+
+def ness_write(db, c):
+	s_time = time.time()
+	for i in range(0, c):
+		key = gen_random_str(16)
+		db.db_add(key, "abcd")
+		if (i % 10000) == 0:
+			sys.stdout.write("\r\x1b[K ....write finished " + i.__str__())
+	 		sys.stdout.flush()
+
+	e_time = time.time()
+	print ""
+	print "---->count:<%i>,cost time:<%i>, %i/sec\n"  %(c, e_time - s_time, c / (e_time - s_time))
+
+
+if __name__ == '__main__':
+	if (len(sys.argv) > 2):
+		if (sys.argv[1] == "write"):
+			db = ness_open("test")
+			ness_write(db, int(sys.argv[2]))
+			db.db_close()
+	else:
+		print "test.py write <count>"
 
