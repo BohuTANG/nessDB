d5c2061d1d66b14091a5ba35037ba02e64904d24
zhaobr@gmail.com <zhaoboran@feinno.com>
2013-01-07 17:17:51 +0800

    Compatible version for nessDB v2.0, no db_scan yet..

--- bindings/haskell/c/free_val.c
+++
@@ -1,7 +0,0 @@
-#include "free_val.h"
-
-void
-free_val(struct slice *val_slice)
-{
-    free(val_slice->data);
-}
--- bindings/haskell/cbits/free_val.h
+++
@@ -1,9 +0,0 @@
-#ifndef FREE_VAL_H
-#define FREE_VAL_H
-
-#include "db.h"
-void free_val(struct slice *val_slice);
-
-#endif /* end of include guard: FREE_VAL_H */
-
-
